d20657dce08d6566653f640f2f8d2650ba097dc5
AshrafulAlam <ashalam610@gmail.com>
2022-04-14 20:02:17 +0600

    second-commit

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1>This is About Windows .Never give up keep hard working</h1>
+            <h1>This is About .Never give up keep hard working</h1>
         </div>
     );
 };
